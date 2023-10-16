3ef64dc276d07aebf4996056c7e0de71660dc050
Dishanthi1 <147810788+Dishanthi1@users.noreply.github.com>
2023-10-16 19:35:56 +0530

    Create factorial 1.py

---
+++ factorial 1.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Input from the user
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    result = factorial(num)
+    print(f"The factorial of {num} is {result}")
